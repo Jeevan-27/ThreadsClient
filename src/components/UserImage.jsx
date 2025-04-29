40c3be794fc8e6c666c1dbf2b649ee4c2cde3b3b
Jeevan-27 <141016628+Jeevan-27@users.noreply.github.com>
2025-04-29 10:54:21 +0530

    Update UserImage.jsx

--- src/components/UserImage.jsx
+++ src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:6001/assets/${image}`}
+        src={`https://threads-app-two-flame.vercel.app/assets/${image}`}
       />
     </Box>
   );
